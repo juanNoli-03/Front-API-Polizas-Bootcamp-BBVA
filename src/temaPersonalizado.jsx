ab049c5bf733ee1709a3acf1262375feadc21f2a
Juan Manuel Noli <juanmanuelnoli03@gmail.com>
2024-11-14 23:03:24 -0300

    Tema personalizado

---
+++ src/temaPersonalizado.jsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const temaPersonalizado = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: '#E8E8E8'
+    }
+  },
+  typography: {
+    fontFamily: "Poppins"
+  },
+  spacing: 8,
+});
